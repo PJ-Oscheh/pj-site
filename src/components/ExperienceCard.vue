e7b179f0c2ffd2030b263c2b2100c425c66f75e8
PJ Oschmann <phillip236.po@gmail.com>
2025-06-04 17:21:32 -0400

    Create exp card

---
+++ src/components/ExperienceCard.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+    employer: string,
+    title: string,
+    location: string,
+    dateOfEmployment: string
+}>();
+
+</script>
+
+<template>
+    <div class="experienceCard">
+
+        <h1 class="employerName">{{ props.employer }}</h1>
+        <h2 class="jobTitle">{{ props.title }}</h2>
+        <span class="workLocation">{{ props.location }}</span>
+        <span class="divider">|</span>
+        <span class="dateOfEmployment">{{ props.dateOfEmployment }}</span><br/>
+
+        <div class="experienceInfo">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+div.experienceCard {
+    background-color: #f3f3f3;
+    box-shadow: 0px 0px 12px #b9b9b9;
+    outline-style: solid;
+    outline-color: #cfcfcf;
+    border-radius: 6px;
+    max-width: 600px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 12px;
+}
+
+div.experienceInfo {
+    margin-top: 12px;
+    margin-left: 6px;
+}
+
+h1.employerName {
+    margin: 6px;
+}
+
+h2.jobTitle {
+    color: #005dce;
+    margin: 6px;
+}
+
+span.workLocation {
+    margin-left: 6px;
+}
+
+/* Separates work location & date of employment */
+span.divider {
+    margin-left: 6px;
+    margin-right: 6px;
+}
+
+span.workLocation, span.dateOfEmployment, span.divider {
+    color: #5e5e5e;
+}
+
+:slotted(ul.ecList) {
+    margin: 0px;
+    margin-top: 3px;
+}
+
+:slotted(ul.ecList li) {
+    margin: 0px;
+}
+
+</style>
---
+++ src/views/PortfolioView.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import ExperienceCard from "@/components/ExperienceCard.vue"
+
+</script>
+
+<template>
+    <div class="portfolioView">
+        <ExperienceCard employer="Current Lighting Solutions" title="Software Developer (Intern)" location="Beachwood, OH" dateOfEmployment="May 2024 - August 2024">
+            <ul class="ecList">
+                <li>Develop Python software for factories to configure LED drivers over DALI interface</li>
+                <ul>
+                    <li>Gain IoT experience</li>
+                    <li>Learn how to interact with hardware and verify changes were applied successfully</li>
+                    <li>Collaborate with factory employees to ensure application requirements are met</li>
+                    <li>Deliver application to factory that produces LED drivers</li>
+                </ul>
+                <li>Create cross-platform mobile app for field engineers to diagnose lighting sensors and controls</li>
+                <ul>
+                    <li>Gain mobile app development experience</li>
+                    <li>Quickly learn Dart and Flutter to start and complete app for use internally by field engineers</li>
+                    <li>Learn how to interact with IoT devices over Bluetooth LE</li>
+                </ul>
+            </ul>
+        </ExperienceCard>
+    </div>
+</template>
+
+<style scoped>
+
+div.portfolioView {
+    padding: 24px;
+}
+
+</style>
