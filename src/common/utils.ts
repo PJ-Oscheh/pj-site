3af4143bd9d35af052e1480ac9a4642d7cf20900
PJ-Oscheh <phillip236.po@gmail.com>
2025-06-06 13:48:42 -0400

    refactor scroll to allow offset

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,6 +1,10 @@
+import { SCROLL_OFFSET } from './constants.ts';
+
 export function scrollToRef(destRef: HTMLElement | null): void {
     if (destRef !== null) {
-        destRef.scrollIntoView({behavior: 'smooth'});
+        const refPos = destRef.getBoundingClientRect().top - SCROLL_OFFSET + window.scrollY;
+        window.scrollTo({top: refPos, behavior: "smooth"});
+        console.log(`Scrolling to ${refPos}`);
     }
     else {
         console.log(`Can't scroll - '${destRef}' is null.`)
